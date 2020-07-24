faac5a8a5bd24eaeb2d9361b56363957b2e7651c
Omar Hatem <omar.hatem95@gmail.com>
2020-07-24 03:12:37 +0200

    Pong 1.0! -Added pixelization of text through applying a nearest neighbour filter as default instead of bilinear. -Added virtualized resolution, to have a smaller resolution inside (magnified) inside our actual resolution, this results in a retro low res feel. -Added the ability to quite game by pressing escape!

--- main.lua
+++ main.lua
@@ -4,15 +4,25 @@
     Pong Remake
 ]]
 
+--Push is a library that allows
+push = require 'push'
+
 --Actual window size
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+--Virtual window size to imitate the low res feel of retro games
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
 --Runs when the game first starts up, only once; used to initialize the game.
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  --This initlizes our virtual resolution, which will be rendered inside our
+  --actual window no matter its dimensions; replaces our typical love.window.setmode()
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -20,12 +30,26 @@ function love.load()
 end
 
 
+function love.keypressed(key)
+    --keys are accessed by string name
+    if key == 'escape' then
+      --built-in function in LÖVE2D to terminate application.
+      love.event.quit()
+    end
+end
+
+
+
 --Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
 function love.draw()
+  --Beging rendering at virtual resolution.
+    push:apply('start')
     love.graphics.printf(
         'Hello Pong!',          -- text to render
         0,                      -- starting X (0 since we're going to center it based on width)
-        WINDOW_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        WINDOW_WIDTH,           -- number of pixels to center within (the entire screen here)
+        VIRTUAL_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
+        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
         'center')               -- alignment mode, can be 'center', 'left', or 'right'
+    --End rendering at virtual resolution.
+    push:apply('end')
 end
